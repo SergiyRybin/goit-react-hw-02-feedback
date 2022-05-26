3369513661e196b498418530053e8b569c31ed38
SergiyRybin <89793296+SergiyRybin@users.noreply.github.com>
2022-05-26 23:39:57 +0300

    Update Fedback.jsx

--- src/components/feedback/Fedback.jsx
+++ src/components/feedback/Fedback.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import Statistics from '../Statistics/Statistics';
+import Statistics from './statistics/Statistics';
 import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
 import Section from '../Section/Section';
 import Notification from '../Notification/Notification';
