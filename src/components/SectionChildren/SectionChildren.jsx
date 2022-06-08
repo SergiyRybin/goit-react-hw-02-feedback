4717b2e56133cf3ff1c9554b835a50ec4bacd7b5
SergiyRybin <serhiobax@gmail.com>
2022-06-08 15:30:49 +0300

    Update SectionChildren.jsx

--- src/components/SectionChildren/SectionChildren.jsx
+++ src/components/SectionChildren/SectionChildren.jsx
@@ -6,13 +6,11 @@ const Section = ({ title, children }) => (
     {title === 'Statistics' ? <h2>{title}</h2> : <h1>{title}</h1>}
     {children}
   </>
-
 );
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
 };
-
 
 export default Section;
